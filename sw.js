574058553e73e807f8e860aaacaad8df7e0b354a
Chris <chris.m.murphy98@gmail.com>
2026-08-17 12:57:45 -0500

    Rate button feedback during auto-play: button press animation + toast confirmation

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Service Worker for Punjabi Flashcards PWA
-const CACHE_NAME = 'punjabi-flashcards-v11';
+const CACHE_NAME = 'punjabi-flashcards-v12';
 const ASSETS = [
   './',
   './index.html',
